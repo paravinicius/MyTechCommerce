4b6efaddd9cc2f160ec4797ec7599e513bc5d9cb
Vinícius Miranda de Menezes <paravinicius@gmail.com>
2024-03-20 20:35:17 -0300

    Criação do repositório do projeto

---
+++ src/main/java/br/com/fiap4/mytechcommerce/MytechcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap4.mytechcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytechcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytechcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap4/mytechcommerce/MytechcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap4.mytechcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytechcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
